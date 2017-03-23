3a17002e6ed071b24648e219f89d9710d06c620c
pikachu <J.Krus@gmx.net>
2017-03-23 17:08:12 +0100

    Init Controllers and their boddies

---
+++ Smartwatchy/src/smartwatchy/controllers/MainController.java
@@ -0,0 +1,6 @@
+package controllers;
+
+public interface MainController {
+	public void buttonPressedA();
+	public void buttonPressedB();
+}
---
+++ Smartwatchy/src/smartwatchy/controllers/NotificationController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+public class NotificationController implements MainController{
+
+	@Override
+	public void buttonPressedA() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void buttonPressedB() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
