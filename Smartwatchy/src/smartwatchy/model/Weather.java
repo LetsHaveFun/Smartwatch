11bfc94cf69d9a5707b0853d7e306ea10ad4df57
pikachu <J.Krus@gmx.net>
2017-03-29 12:53:03 +0200

    Update to template

--- Smartwatchy/src/smartwatchy/model/Weather.java
+++ Smartwatchy/src/smartwatchy/model/Weather.java
@@ -2,18 +2,26 @@ package model;
 
 public class Weather {
 	
-	public String description;
+	public int WeatherID;
 	public int temperature;
 	
-	public Weather(String weathertmp, int temperaturetmp)
+	public Weather(int WeatherIDTMP, int temperaturetmp)
 	{
-		description = weathertmp;
+		WeatherID = WeatherIDTMP;
 		temperature = temperaturetmp;
 	}
 	
 	public Weather GetWeather()
 	{
 		return this;
+	}
+	public int GetWeatherWeatherID()
+	{
+		return WeatherID;
+	}
+	public int GetWeatherTemperature()
+	{
+		return temperature;
 	}
 
 }
